98e0b81867395c81401efd52552afae4d5da6719
iago <xxiagointer@gmail.com>
2021-05-14 01:06:56 -0300

    validation for the neighborhood and local search

--- atividade_graph_coloring.py
+++ atividade_graph_coloring.py
@@ -1,10 +1,8 @@
 from collections import Counter
-
-
-
+import secrets
 def import_data(path_to_file):
     graph_matrix = [[]]
-    vertice_color = []
+    vertices_color = []
     size = 0
     with open(path_to_file) as raw_numbers:
         for line in raw_numbers: 
@@ -16,16 +14,62 @@ def import_data(path_to_file):
                 graph_matrix[v2-1][v1-1]=1
             if line_type == 'p':
                 size = int(list(map(str, line.split()))[2])
-                vertice_color = [i for i in range(size)]
+                vertices_color = [i for i in range(size)]
                 graph_matrix = [[0 for j in range(size) ] for i in range(size)] 
         raw_numbers.close()
-    return (graph_matrix, vertice_color)
+    return (size, graph_matrix, vertices_color)
 
+"""###################### Iterated Local Search #######################
+"""
+def cost_function(vertices_color):
+    return len(Counter(vertices_color).values())
 
-def cost_function(vertice_color):
-    return len(Counter(vertice_color).values())
+def validate_neighbor_candidate(graph_matrix, neighbor_candidate, vertex_original_color, vertex_color_changed):
+    color = neighbor_candidate[vertex_original_color]
+    i = 0
+    for vertex in graph_matrix[vertex_color_changed]:
+        if vertex == 1 and color == neighbor_candidate[i]:
+            return False
+        i += 1
+    return True
 
+def local_search(size, history, solution, graph_matrix):
+    cost_of_neighbors = []
+    first = 0
+    #find first, this one will be the index for mapping the neighbors
+    while first < size:
+        if history[first] == 0: #immutable memory value
+            first += 1
+            continue
+        else:
+            break
+    #find the cost of all valid neighbors of colors
+    i = first + 1
+    index_min_cost = 0
+    while i < size:
+        if history[i] == 1:
+            neighbor_candidate = solution.copy()
+            neighbor_candidate[i] = neighbor_candidate[first] #trying to reduce colors
+            if validate_neighbor_candidate(graph_matrix, neighbor_candidate, first, i):
+                cost = cost_function(neighbor_candidate)
+                cost_of_neighbors.append(cost)
+                if min(cost_of_neighbors) == cost: # keep track of minimum cost
+                    index_min_cost = i
+        i += 1
+    if len(cost_of_neighbors) > 0:
+        minimum_cost = min(cost_of_neighbors)
+        new_solution = solution.copy()
+        new_solution[index_min_cost] = new_solution[first]
+        new_history = history.copy()
+        new_history[first]=0
+        new_history[index_min_cost]=0
+        return (new_history, minimum_cost, new_solution)
+    return (history, cost_function(solution), solution)
 
-#print(import_data("./graph_coloring/dsjc250.5.col")[0])
-print(cost_function(import_data("./graph_coloring/teste.txt")[1]))
-#import_data("./graph_coloring/dsjc250.5.col")
+def perturbation(size, history, solution, graph_matrix):
+    #available_colors = list(set(solution))
+     
+size, graph, trivial_solution = import_data("./graph_coloring/teste.txt")
+s0 = local_search(size, [1 for i in range(size)], trivial_solution, graph)
+print(s0[1])
+print(s0[2])
